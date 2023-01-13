1f90af609939434c7279b6b7aefcfffe86a2ffbc
VikasPushkar1 <vikaspushkar9354@gmail.com>
2023-01-13 14:42:31 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
+import java.util.Scanner;
+
 public class Main {
-  
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        RWOnly obj=new RWOnly();
+
+        // 'name' has private access in 'com.driver.RWOnly'
+        obj.setName(sc.nextLine());
+        System.out.println(obj.getName());
+    }
 }
